2bd68985a6e36773bb8da342020fda6998d7dd24
juanperrin <57192849+juanperrin@users.noreply.github.com>
2020-04-26 15:51:20 +0200

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -10,10 +10,9 @@ x = [1, 2, 3, 4]
 y = [1, 2, 3, 4]
 source = ColumnDataSource(data={'x': x, 'y': y})
 
-data = pd.read_csv(join(dirname(__file__), 'data/feuille de saisie METEO mois par mois.csv'), None, header = (0,1)) # header dans les deux premières lignes de chaque onglet
+data = pd.read_excel(join(dirname(__file__), 'data/feuille de saisie METEO mois par mois.xlsx'), None, header = (0,1))
 
 plot = figure(plot_width = 800, plot_height=300, x_axis_type='datetime')
-    
 glyph = VBar(x='x', bottom=0, top='y', width=1 ,line_alpha=0.1, fill_color="#6599ed")
     
 plot.add_glyph(source, glyph)
